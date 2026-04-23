2ddea17841f8c549694d313a976a2704e0f9622d
Holger <holger.gevers@gmail.com>
2026-04-23 02:32:19 +0200

    style(ide): theme dockview to match existing dark palette

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "dockview-react/dist/styles/dockview.css";
+import "./styles/dockview-theme.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
